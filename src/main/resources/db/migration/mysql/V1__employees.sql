8b2a1cd49edff158736a11460051040ee830b2a4
Gieandes Silva <contato@gieandessilva.com>
2020-03-05 19:42:18 -0300

    Retornando o mysql como banco default

--- src/main/resources/db/migration/mysql/V1__employees.sql
+++ src/main/resources/db/migration/mysql/V1__employees.sql
@@ -1,9 +1,17 @@
-CREATE TABLE employees (
-  id serial PRIMARY KEY NOT NULL,
-  first_name varchar(255) NOT NULL,
-  last_name varchar(255) NOT NULL,
-  role varchar(60) NOT NULL,
-  create_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  deleted_at TIMESTAMP NULL
-);
+CREATE TABLE employees
+(
+    id         bigint(20)   NOT NULL,
+    first_name varchar(255) NOT NULL,
+    last_name  varchar(255) NOT NULL,
+    role       varchar(60)  NOT NULL,
+    create_at  TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    deleted_at TIMESTAMP    NULL
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
+
+ALTER TABLE employees
+    ADD PRIMARY KEY (id);
+
+ALTER TABLE employees
+    MODIFY id bigint(20) NOT NULL AUTO_INCREMENT;
