ce3423b82ba1cf562555a8a953708dfd47625965
Gieandes Silva <contato@gieandessilva.com>
2020-03-11 00:16:08 -0300

    Oauth adicionado

--- src/main/resources/db/migration/mysql/V2__roles.sql
+++ src/main/resources/db/migration/mysql/V2__roles.sql
@@ -1,9 +1,7 @@
-CREATE TABLE employees
+CREATE TABLE roles
 (
     id         bigint       NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    first_name varchar(255) NOT NULL,
-    last_name  varchar(255) NOT NULL,
-    role       varchar(60)  NOT NULL,
+    name       varchar(255) NOT NULL,
     create_at  TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     deleted_at TIMESTAMP    NULL
